1afe24cc525b619af6f9435af9e858e6be595714
Julia Filippova <julia.94@bk.ru>
2022-08-03 01:36:51 +0300

    feat: add animation appearance and rotate

---
+++ global/js/animate-flags.js
@@ -0,0 +1,24 @@
+const flags = document.querySelectorAll(".flag");
+const flagBlock = document.querySelector(".flags");
+let allInterval = 3000;
+
+const appearanceOfFlags = () => {
+  const interval = 400; 
+  let increment = 1;
+  flags.forEach((flag, index) => {
+    const addId = setTimeout(() => {
+      flag.id = `flag${index}`;
+      clearTimeout(addId);
+    }, interval * increment);
+    increment = increment + 1;
+  });
+};
+
+setTimeout(appearanceOfFlags, allInterval);
+allInterval += (400*flags.length);
+console.log(allInterval)
+const step = () => {
+  flagBlock.style.animation = "rotateCircle 7s linear infinite";
+};
+setTimeout(step, allInterval);
+
